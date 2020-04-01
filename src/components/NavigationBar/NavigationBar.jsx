cc035d317b1606e61773c77a410868268bc66016
msobalvarro <mssrsobalvarro1997@gmail.com>
2020-04-01 12:27:03 -0600

    comming soon profile

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -20,7 +20,7 @@ const NavigationBar = () => {
             <div className="content-links">
                 <Link to="/" className={(location === '#/') ? 'active' : ''}>Dashboard</Link>
                 <Link to="/sponsors" className={(location === '#/sponsors') ? 'active' : ''}>Comisiones</Link>
-                <Link to="/profile" className={(location === '#/profile') ? 'active' : ''}>{globalStorage.username}</Link>
+                <a to="#" className="comming-soon">{globalStorage.username}</a>
                 <a href="#" onClick={LogOut}>Cerrar sesion</a>
             </div>
         </nav>
