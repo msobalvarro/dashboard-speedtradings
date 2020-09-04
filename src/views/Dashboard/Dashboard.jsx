9a835d8ecc6953e751615aae82a7b8352a0b2bef
Harold Espinoza <haroldesptru@gmail.com>
2020-09-04 14:28:13 -0600

    Añadida vista para visualizar todas las transacciones en un modal

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -13,6 +13,7 @@ import BuyPlan from "../../components/BuyPlan/BuyPlan"
 import HeaderDashboard from "../../components/HeaderDashboard/HeaderDashboard"
 import NavigationBar from "../../components/NavigationBar/NavigationBar"
 import MobileMessage from "../../components/Mobile/Mobile"
+import DashboardAllReports from "../../components/DashboardAllReports/DashboardAllReports"
 import Swal from "sweetalert2"
 import { optionsChartDashboard, Petition, calculateCryptoPriceWithoutFee } from "../../utils/constanst"
 import { useSelector } from "react-redux"
@@ -20,26 +21,31 @@ import { useSelector } from "react-redux"
 
 const DashboardDetails = ({ data = {}, type = "" }) => {
     const [showMoreContent, setShow] = useState(false)
-    const [cryptoPrices, setCryptoPrices] = useState({ BTC: null, ETH: null })
     const labels = []
     const series = []
-
     const showMoreRef = useRef(null)
+
+    /**Costante que almacena el total de las ganancias */
+    const amountSumArr = []
+
+    /** Destructuración de las data recibida para separar el historial y la info */
+    const {history, info, prices={ BTC: null, ETH: null }} = data;
 
     const showMore = () => {
         setShow(true)
 
-        showMoreRef.current.scrollIntoView()
+        //showMoreRef.current.scrollIntoView()
     }
 
     /**Creamos el algoritmo para ordenar los datos de la grafica */
-    if (data.history.length > 0) {
+    if (history.length > 0) {
 
         for (let index = 0; index < data.history.length - 5; index++) {
             const item = data.history[index]
 
             labels.push(moment(item.date).format('dddd'))
             series.push(item.amount)
+            amountSumArr.push(item.amount)
         }
     }
 
@@ -50,9 +56,6 @@ const DashboardDetails = ({ data = {}, type = "" }) => {
         series: [series]
     }
 
-    /**Costante que almacena el total de las ganancias */
-    const amountSumArr = []
-
     /** Calcula el total de la profit-table */
     const calculateTotalProfitTable = (data) => {
         const sum = data.reduce((a, b) => a + b, 0)
@@ -61,42 +64,16 @@ const DashboardDetails = ({ data = {}, type = "" }) => {
         return total
     }
 
-    /**
-     * Obtiene los precios de la coinmarketcap
-     * */
-    const getAllPrices = async () => {
-        try {
-            const { data } = await Petition.get("/collection/prices")
-
-            if (data.error) {
-                throw String(data.message)
-            } else {
-                const { BTC, ETH } = data
-
-                setCryptoPrices({ BTC, ETH })
-            }
-
-        } catch (error) {
-            Swal.fire("Ha ocurrdio un error", error.toString(), "warning")
-        }
-    }
-
-    // Se cargan los precios de las monedas
-    useEffect(() => {
-        getAllPrices()
-    }, [data, type])
-
     // Se indica la lista de transacciones a listar
-
     return (
         <>
             {
-                data.info.approved === 0 &&
+                info.approved === 0 &&
                 <h1 className="message-disabled">Tu plan se activara cuando sea verificado</h1>
             }
 
-            <div className={data.info.approved === 0 ? 'disabled' : ''}>
-                <HeaderDashboard type={type} disabled={data.info.approved === 0} idInvestment={data.info.id_investment} amount={(data.info.amount)} amountToday={data.info.total_paid} />
+            <div className={info.approved === 0 ? 'disabled' : ''}>
+                <HeaderDashboard type={type} disabled={info.approved === 0} idInvestment={info.id_investment} amount={(info.amount)} amountToday={info.total_paid} />
 
                 <div className="card chart">
                     <ChartistGraph data={dataChart} options={optionsChartDashboard} type="Line" />
@@ -107,7 +84,7 @@ const DashboardDetails = ({ data = {}, type = "" }) => {
                         <div className="col">
                             <h2 className="big">
                                 {
-                                    moment(data.info.start_date).format('MMM. D, YYYY')
+                                    moment(info.start_date).format('MMM. D, YYYY')
                                 }
                             </h2>
                             <span>Fecha de inicio</span>
@@ -116,7 +93,7 @@ const DashboardDetails = ({ data = {}, type = "" }) => {
                         <div className="col yellow">
                             <h2 className="big">
                                 {
-                                    (data.info.amount_to_win) + ' ' + type.toUpperCase()
+                                    (info.amount_to_win) + ' ' + type.toUpperCase()
                                 }
                             </h2>
                             <span>Monto a ganar</span>
@@ -127,8 +104,8 @@ const DashboardDetails = ({ data = {}, type = "" }) => {
                         <div className="col">
                             <h2>
                                 {
-                                    data.info.last_pay !== null
-                                        ? (data.info.last_pay) + ' ' + type.toUpperCase()
+                                    info.last_pay !== null
+                                        ? (info.last_pay) + ' ' + type.toUpperCase()
                                         : <span>(Sin reportes)</span>
                                 }
                             </h2>
@@ -138,9 +115,9 @@ const DashboardDetails = ({ data = {}, type = "" }) => {
                         <div className="col yellow">
                             <h2>
                                 {
-                                    data.info.amount_rest !== null
-                                        ? (data.info.amount_rest) + ' ' + type.toUpperCase()
-                                        : (data.info.amount_to_win) + ' ' + type.toUpperCase()
+                                    info.amount_rest !== null
+                                        ? (info.amount_rest) + ' ' + type.toUpperCase()
+                                        : (info.amount_to_win) + ' ' + type.toUpperCase()
                                 }
                             </h2>
                             <span>Saldo pendiente</span>
@@ -150,7 +127,7 @@ const DashboardDetails = ({ data = {}, type = "" }) => {
 
                 <div className="card profit-table">
                     {
-                        data.history.length !== 0 &&
+                        history.length !== 0 &&
                         <div className="table">
                             <div className="header">
                                 <span>Fecha</span>
@@ -160,17 +137,15 @@ const DashboardDetails = ({ data = {}, type = "" }) => {
                             </div>
                             <div className="body">
                                 {
-                                    data.history.map((item, index) => (
+                                    history.map((item, index) => (
                                         <div className={`row ${moment().get('week') !== moment(item.date).get('week') ? 'paymented' : ''}`} key={index}>
                                             <span>{moment(item.date).format('MMM. D, YYYY')}</span>
                                             <span>{item.percentage}%</span>
                                             <span>{item.amount}</span>
                                             <span>
                                                 $ {
-                                                    cryptoPrices.BTC !== null
-                                                        ? (type === 'btc')
-                                                            ? calculateCryptoPriceWithoutFee(cryptoPrices.BTC.quote.USD.price, item.amount)
-                                                            : calculateCryptoPriceWithoutFee(cryptoPrices.ETH.quote.USD.price, item.amount)
+                                                    prices.BTC !== null
+                                                        ? calculateCryptoPriceWithoutFee(prices[type.toUpperCase()], item.amount)
                                                         : 0
                                                 }
                                             </span>
@@ -178,10 +153,10 @@ const DashboardDetails = ({ data = {}, type = "" }) => {
                                     ))
                                 }
                             </div>
-                            <div className={`footer ${(amountSumArr.length > 7 && !showMoreContent) ? 'more' : ''}`}>
+                            <div className={`footer ${(!showMoreContent) ? 'more' : ''}`}>
 
                                 {
-                                    (amountSumArr.length > 7 && !showMoreContent) &&
+                                    (!showMoreContent) &&
                                     <span className="show-more" onClick={showMore} ref={showMoreRef}>
                                         Mostrar todo
                                     </span>
@@ -195,10 +170,8 @@ const DashboardDetails = ({ data = {}, type = "" }) => {
                                         }
                                         {
                                             ` ($ ${
-                                            cryptoPrices.BTC !== null
-                                                ? (type === 'btc')
-                                                    ? calculateCryptoPriceWithoutFee(cryptoPrices.BTC.quote.USD.price, calculateTotalProfitTable(amountSumArr))
-                                                    : calculateCryptoPriceWithoutFee(cryptoPrices.ETH.quote.USD.price, calculateTotalProfitTable(amountSumArr))
+                                            prices.BTC !== null
+                                                ? calculateCryptoPriceWithoutFee(prices[type.toUpperCase()], calculateTotalProfitTable(amountSumArr))
                                                 : 0
                                             })`
                                         }
@@ -209,10 +182,15 @@ const DashboardDetails = ({ data = {}, type = "" }) => {
                     }
 
                     {
-                        data.history.length === 0 &&
+                        history.length === 0 &&
                         <h2 className="empty">Aun no hay historial de ganancias</h2>
                     }
                 </div>
+
+                {
+                    showMoreContent &&
+                    <DashboardAllReports type={type} prices={prices} onClose={_ => setShow(false)}/>
+                }
             </div>
         </>
     )
@@ -239,26 +217,24 @@ const Dashboard = () => {
             // Get data BTC
             const { data: dataBTC } = await Petition.get('/dashboard/1', headers)
 
-            console.log(dataBTC)
-
             // verificamos si hay un error al cargar los datos
             if (dataBTC.error) {
                 throw String(dataBTC.message)
-            } else {
+            } else if (Object.keys(dataBTC).length > 0) {
                 setDataDashboardBTC(dataBTC)
             }
 
+            // Get data BTC
+            //const { data: dataTest } = await Petition.get('/dashboard/all-reports/1', headers)
 
             // Get data ETH
             const { data: dataETH } = await Petition.get('/dashboard/2', headers)
 
-            console.log(dataETH)
-
             // verificamos si hay un error al cargar los datos
             if (dataETH.error) {
                 throw String(dataETH.message)
-            } else {
-                setDataDashboardETH(dataETH)
+            } else if (Object.keys(dataETH).length > 0) {
+                setDataDashboardETH(dataETH);
             }
 
             setLoader(false)
@@ -292,40 +268,26 @@ const Dashboard = () => {
                 <div className="content-dashboard">
                     <div className="content">
                         {
-                            (dataDashoardBTC.info !== null) &&
-                            <>
-                                {
-                                    (dataDashoardBTC.info.amount !== null) &&
-                                    <DashboardDetails type="btc" data={dataDashoardBTC} />
-                                }
-
-
-                                {
-                                    dataDashoardBTC.info.amount === null &&
-                                    <BuyPlan onBuy={ConfigurateComponent} idCrypto={1} />
-                                }
-                            </>
+                            (dataDashoardBTC.info !== null && dataDashoardBTC.info.amount !== null) &&
+                            <DashboardDetails type="btc" data={dataDashoardBTC} />
                         }
 
+                        {
+                            dataDashoardBTC.info === null &&
+                            <BuyPlan onBuy={ConfigurateComponent} idCrypto={1} />
+                        }
                     </div>
 
                     <div className="content">
-
                         {
-                            dataDashoardETH.length > 0 &&
-                            <>
-                                {
-                                    (dataDashoardETH.info?.amount !== null) &&
-                                    <DashboardDetails type="eth" data={dataDashoardETH} />
-                                }
-
-                                {
-                                    dataDashoardETH.info?.amount === null &&
-                                    <BuyPlan onBuy={ConfigurateComponent} idCrypto={2} />
-                                }
-                            </>
+                            (dataDashoardETH.info !== null && dataDashoardETH.info?.amount !== null) &&
+                            <DashboardDetails type="eth" data={dataDashoardETH} />
                         }
 
+                        {
+                            dataDashoardETH.info === null &&
+                            <BuyPlan onBuy={ConfigurateComponent} idCrypto={2} />
+                        }
                     </div>
                 </div>
             }
