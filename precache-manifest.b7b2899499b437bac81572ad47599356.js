2492cba8e47e1926c4bddd795c50d39ada72c9a6
msobalvarro <mssrsobalvarro1997@gmail.com>
2020-11-24 08:50:05 -0600

    Updates

---
+++ precache-manifest.b7b2899499b437bac81572ad47599356.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ac61b729c5539b93f1555bc158daf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00d4647614ae87f67661",
+    "url": "/static/css/2.27f7052c.chunk.css"
+  },
+  {
+    "revision": "87dc1fdfac095ee97f5a",
+    "url": "/static/css/main.ed2e4fb0.chunk.css"
+  },
+  {
+    "revision": "00d4647614ae87f67661",
+    "url": "/static/js/2.b91afae0.chunk.js"
+  },
+  {
+    "revision": "7cb3bb1246e86ff3cb76a4bbcb44dc43",
+    "url": "/static/js/2.b91afae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87dc1fdfac095ee97f5a",
+    "url": "/static/js/main.725d64e9.chunk.js"
+  },
+  {
+    "revision": "6a4e61de14897655b035",
+    "url": "/static/js/runtime-main.d918a19f.js"
+  },
+  {
+    "revision": "60352b684297568f90cddf10c248e9c1",
+    "url": "/static/media/AlyCoin.60352b68.png"
+  },
+  {
+    "revision": "ee5c670b11367eb159f8970fdb7a293c",
+    "url": "/static/media/AlyPay.ee5c670b.png"
+  },
+  {
+    "revision": "1fc9bccef912b5693d751e3bfe524858",
+    "url": "/static/media/AlySystem-banner.1fc9bcce.png"
+  },
+  {
+    "revision": "e40570a322c8676a5c07b3630c4cfee5",
+    "url": "/static/media/GothamRounded-Book.e40570a3.otf"
+  },
+  {
+    "revision": "ae8f19f441b335c3fda30febf5069c5a",
+    "url": "/static/media/GothamRounded_Bold.ae8f19f4.ttf"
+  },
+  {
+    "revision": "05e4ab0a8d48fca7666b1728a2c97bf6",
+    "url": "/static/media/SpeedTradings.05e4ab0a.png"
+  },
+  {
+    "revision": "9c2e8138281fd9313c63eea61c02997f",
+    "url": "/static/media/add.9c2e8138.svg"
+  },
+  {
+    "revision": "f4355ef818221e3b24d93c6b553e745f",
+    "url": "/static/media/alypay-logo.f4355ef8.png"
+  },
+  {
+    "revision": "9642f681c021ef0dd1d88150136d14d9",
+    "url": "/static/media/alysystem.9642f681.png"
+  },
+  {
+    "revision": "dda6dba8a75ab205939e24f32304411a",
+    "url": "/static/media/appstore.dda6dba8.png"
+  },
+  {
+    "revision": "a77a807ef928fbe2dca0764b41f5f286",
+    "url": "/static/media/arrow-back.a77a807e.svg"
+  },
+  {
+    "revision": "379a25543e1473e57da58243676d30be",
+    "url": "/static/media/arrow-forward.379a2554.svg"
+  },
+  {
+    "revision": "6b2c82b933138c730774a30a90e2a3d0",
+    "url": "/static/media/astronaut.6b2c82b9.svg"
+  },
+  {
+    "revision": "9d7025640a0dabff8581c9a97b8d3f33",
+    "url": "/static/media/astronaut.9d702564.png"
+  },
+  {
+    "revision": "e816e238db002c8e49b26f6bcca019c5",
+    "url": "/static/media/bitcoin.e816e238.svg"
+  },
+  {
+    "revision": "6724e28006e6914078b7503f0dc09f56",
+    "url": "/static/media/cancel.6724e280.svg"
+  },
+  {
+    "revision": "dcd9346695b2bb04b1d4c99fa0b7266c",
+    "url": "/static/media/check-circular-button.dcd93466.svg"
+  },
+  {
+    "revision": "ec9c38dec5dfe466ee94e565e430a261",
+    "url": "/static/media/earth.ec9c38de.svg"
+  },
+  {
+    "revision": "05cc6d5c668c68ef463c19a4bc98e4ee",
+    "url": "/static/media/enterprise.05cc6d5c.svg"
+  },
+  {
+    "revision": "738f77a545442a1b7dd941686faf0f03",
+    "url": "/static/media/ether.738f77a5.svg"
+  },
+  {
+    "revision": "b1906a9365e468c14461cd6e60b8560a",
+    "url": "/static/media/information.b1906a93.svg"
+  },
+  {
+    "revision": "b7dea37fe80a8c8167078c55e9c6323d",
+    "url": "/static/media/itunes.b7dea37f.png"
+  },
+  {
+    "revision": "a55e71aea268e8cde6d85a38b6ea8578",
+    "url": "/static/media/logo.a55e71ae.png"
+  },
+  {
+    "revision": "9fae76456c1a671601cb47417f978582",
+    "url": "/static/media/moon.9fae7645.svg"
+  },
+  {
+    "revision": "0b9235c3b9875ba7995fef1d5b4b287a",
+    "url": "/static/media/placeholder-profile.0b9235c3.jpg"
+  },
+  {
+    "revision": "5d3baf62bbf6bab42a9eeced959746c8",
+    "url": "/static/media/play-store.5d3baf62.png"
+  },
+  {
+    "revision": "ec88b932f8e1d44484889e22eaa5cf9b",
+    "url": "/static/media/playstore.ec88b932.png"
+  },
+  {
+    "revision": "7461c5acbe983fb4c6306a64da2d976a",
+    "url": "/static/media/rocket.7461c5ac.svg"
+  },
+  {
+    "revision": "7aec079e36efeb6502e46951c81006f2",
+    "url": "/static/media/save.7aec079e.svg"
+  },
+  {
+    "revision": "d50eefc7551824a5a24544931d1dad47",
+    "url": "/static/media/upload.d50eefc7.svg"
+  },
+  {
+    "revision": "57eda5a1cf02402cbc2101685cda9037",
+    "url": "/static/media/user.57eda5a1.svg"
+  }
+]);
