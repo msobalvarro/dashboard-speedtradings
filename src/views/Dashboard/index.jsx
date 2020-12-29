83440f223d581dcbbd287e38646f0d42e0a612c1
Cesar Garay <cgaray.aguilar97@gmail.com>
2020-12-29 08:47:22 -0600

    Grafica de dashboard finalizada

--- src/views/Dashboard/index.jsx
+++ src/views/Dashboard/index.jsx
@@ -5,10 +5,18 @@ import ActivityIndicator from '../../components/ActivityIndicator/Activityindica
 import Swal from 'sweetalert2'
 import { Petition } from '../../utils/constanst'
 import { useSelector } from 'react-redux'
+import { Line } from 'react-chartjs-2'
+import moment from 'moment'
+
+import InfiniteCalendar from 'react-infinite-calendar'
+
+import DateRangePicker from 'react-daterange-picker'
+
+import 'react-daterange-picker/dist/css/react-calendar.css'
 
 //Importar estilos
 import './styles.scss'
-
+import 'react-infinite-calendar/styles.css'
 const Dashboard = () => {
   const storage = useSelector(({ globalStorage }) => globalStorage)
 
@@ -24,6 +32,64 @@ const Dashboard = () => {
     info: null,
     history: null,
   })
+
+  const [chartData, setChartData] = useState({})
+
+  const getLabels = _data => {
+    if (!_data) return false
+
+    //Obtener los labels
+    return _data.map(item => moment(item.date).format('ddd DD MMM'))
+  }
+
+  const getSeries = _data => {
+    if (!_data) return false
+
+    //Obtener los valores para las graficas
+    return _data.map(item => item.amount)
+  }
+
+  const loadChart = () => {
+    setChartData({
+      labels: getLabels(dataDashoardBTC.history),
+      datasets: [
+        {
+          label: 'Bitcoin',
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: '#ffcb08',
+          borderColor: '#ffcb08',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: '#ffcb08',
+          pointBackgroundColor: '#ffcb08',
+          pointBorderWidth: 8,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: getSeries(dataDashoardBTC.history),
+        },
+        {
+          label: 'Ethereum',
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: '#9ed3da',
+          borderColor: '#9ed3da',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBackgroundColor: '#9ed3da',
+          pointBorderColor: '#9ed3da',
+          pointBorderWidth: 8,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: getSeries(dataDashoardETH.history),
+        },
+      ],
+    })
+  }
 
   const ConfigurateComponent = async () => {
     try {
@@ -67,19 +133,53 @@ const Dashboard = () => {
   }, [])
 
   useEffect(() => {
-    console.log(dataDashoardBTC)
-  }, [dataDashoardBTC])
+    //Cargamos los datos de la grafica
+    loadChart()
+  }, [dataDashoardBTC, dataDashoardETH])
 
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: false,
+          },
+        },
+      ],
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    tooltips: {
+      mode: 'point',
+    },
+  }
+
+  const withRange = () => {}
   return (
-    <section className="Dashboard">
+    <div>
       <NavigationBar />
-      {loader && <ActivityIndicator size={128} className="loader-dashboard" />}
-      <main className="plan__container">
-        <DashboardDetails plan="bitcoin" data={dataDashoardBTC.info} />
-        <DashboardDetails plan="ethereum" data={dataDashoardETH.info} />
-      </main>
-      <section className="graphic__container"></section>
-    </section>
+      <section className="Dashboard">
+        {loader && (
+          <div className="center__element">
+            <ActivityIndicator size={128} className="loader-dashboard" />
+          </div>
+        )}
+        <main className="plan__container">
+          <DashboardDetails plan="bitcoin" data={dataDashoardBTC.info} />
+          <DashboardDetails plan="ethereum" data={dataDashoardETH.info} />
+        </main>
+
+        <section className="graphic__container">
+          <Line data={chartData} options={options} />
+        </section>
+
+        <section className="profits__container">
+          <h2>Historial</h2>
+          <DateRangePicker />
+          <input type="date" />
+        </section>
+      </section>
+    </div>
   )
 }
 
