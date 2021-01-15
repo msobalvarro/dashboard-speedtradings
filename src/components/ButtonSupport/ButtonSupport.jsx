f5346eba70a4ee93c7c4a5b02ff4b210c05bfb5d
Cesar Garay <cgaray.aguilar97@gmail.com>
2021-01-15 11:55:45 -0600

    Estilos del boton de soporte modificado

--- src/components/ButtonSupport/ButtonSupport.jsx
+++ src/components/ButtonSupport/ButtonSupport.jsx
@@ -6,50 +6,20 @@ import './ButtonSupport.scss'
 import support from '../../static/images/support.png'
 
 const ButtonSupport = () => {
-  // Estado par controlar la visivilidad del botón de soporte
-  const [visible, setVisible] = useState(false)
-
-  const ref = useRef(null)
-
-  // Función para mostrar el botón de soporte
-  const onMouseHover = () => {
-    setVisible(true)
-  }
-
-  // Función para ocultar el botón de soporte
-  const asyncHidden = () =>
-    new Promise(() => setTimeout(() => setVisible(false), 250))
-
-  // Cuando el puntero del mouse deja de estar sobre el botón, se esperan 1.5 segundos y se activa la función para ovultar el botón de soporte
-  const onMouseLeave = async () => {
-    if (ref !== null) {
-      ref.current.style.opacity = 0
-
-      asyncHidden().then(() => {
-        ref.current.style.opacity = 1
-      })
-    }
-  }
-
   return (
-    <>
+    <div className="container-support">
       <a
         target="_blank"
         rel="noopener noreferrer"
-        href="https://wa.me/+50660727720"
+        href="https://wa.me/+50585570529"
         className="button-support"
-        onMouseOver={onMouseHover}
-        onMouseLeave={onMouseLeave}
       >
         <WhatsappIcon className="support__icon" />
       </a>
-
-      {visible && (
-        <div ref={ref} className={`container-support`}>
-          <span className="label">Escribe un mensaje a soporte tecnico</span>
-        </div>
-      )}
-    </>
+      <span className="label tooltip">
+        Escribe un mensaje a soporte tecnico
+      </span>
+    </div>
   )
 }
 
